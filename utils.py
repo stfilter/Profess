18bd58f86ce414de5a6034d1dffa018de1207e88
stfilter <123819448+stfilter@users.noreply.github.com>
2023-03-08 23:12:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -427,7 +427,7 @@ def humanbytes(size):
 
 async def get_shortlink(link):
 
-    url = f'{SHORT_URL}/api'
+    url = f'https://mdisk.info/api'
     params = {
       'api': SHORT_API,
       'url': link,
